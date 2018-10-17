9d7d832b9a892d89108fa911843741e26a28c7c5
Joseph L. Ngoma <testing.jo.wtc@gmail.com>
2018-10-17 10:53:53 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env/bash
+
+#         _
+#        (_)___  ____ _____  ____ ___  ____ _
+#       / / __ \/ __ `/ __ \/ __ `__ \/ __ `/
+#      / / / / / /_/ / /_/ / / / / / / /_/ /
+#   __/ /_/ /_/\__, /\____/_/ /_/ /_/\__,_/
+#  /___/      /____/
+
+#update submodule installation
+git submodule update --init --recursive
